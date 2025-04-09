4fc03bf58084fd9ab5619fdb6dbcb0e2bd22368f
yosshyjungle <yosshyjungle@gmail.com>
2025-04-09 15:28:38 +0900

    3rd commit

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
+export const CartContext = createContext();
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+const CartProvider = ({children}) => {
+  const [ cart, setCart ] = useState([]);
+
+  const addToCart = (product, id) => {
+    const newItem = {...product, amount: 1};
+    console.log(newItem);
+    // console.log(`item ${product.title} added to the cart`);
+  }
+  return (
+    <CartContext.Provider value={{ addToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
 };
 
-export default CartContext;
+export default CartProvider;
