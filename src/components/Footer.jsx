83da38864d4777be59abab880963db15f4c36041
yosshyjungle <yosshyjungle@gmail.com>
2025-04-11 13:18:12 +0900

    6th commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-primary py-12'>
+      <div className="container mx-auto">
+        <p className='text-white text-center'>Copyright &copy; Nissho Ecommerce Shop 2024. All rights reserved.</p>
+      </div>
+    </footer>
+
+  );
 };
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { ProductContext } from '../contexts/ProductContext';
 import Product from '../components/Product';
+import Hero from '../components/Hero';
 
 const Home = () => {
   const { products } = useContext(ProductContext);
@@ -12,15 +13,18 @@ const Home = () => {
   });
   // console.log(filteredProducts);
   return(
-    <section className='p-16'>
-      <div className='container mx-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] ma-w-sm mx-auto md:max-w-none md:mx-0'>
-          {filteredProducts.map((product) => {
-            return <Product product={product} key={product.id} />  
-          })}
+    <div>
+      <Hero />
+      <section className='p-16'>
+        <div className='container mx-auto'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] ma-w-sm mx-auto md:max-w-none md:mx-0'>
+            {filteredProducts.map((product) => {
+              return <Product product={product} key={product.id} />  
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
